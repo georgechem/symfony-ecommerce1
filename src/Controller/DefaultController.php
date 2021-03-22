421164d868c1e7c49bb62b163e067fb07afb6c27
root <grzegorzchrymecki@php-developer.ie>
2021-03-22 20:09:23 +0000

    ok

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,23 +15,12 @@ class DefaultController extends AbstractController
      */
     public function index(FilesystemService $filesystemService): Response
     {
-        $files = $filesystemService->getFilesFromDirectory('/src/Entity');
+        $files = $filesystemService
+            ->getFilesFromDirectory('/src/Entity');
 
-        dd($files);
-
-        $user = new User();
-
-        $object = get_class($user);
-
-        //$em =$this->container->get('doctrine')->getEntityManager();
-
-        $entityName = $this->getDoctrine()
-            ->getManager()
-            ->getMetadataFactory()
-            ->getMetadataFor($object);
 
         return $this->render('default/index.html.twig', [
-            'object' => $object,
+           'files'=>$files
 
         ]);
     }
--- src/Service/FilesystemService.php
+++ src/Service/FilesystemService.php
@@ -15,7 +15,7 @@ class FilesystemService
         $this->rootDir = $container->getParameter('kernel.project_dir');
     }
 
-    public function getFilesFromDirectory($dir)
+    public function getFilesFromDirectory($dir, $extension = false)
     {
         $iterator = new \DirectoryIterator($this->rootDir . $dir);
 
@@ -23,11 +23,26 @@ class FilesystemService
         while($iterator->valid()){
             $name = $iterator->getBasename();
 
-            if($name !== '.' && $name !== '..' && $name !== '.gitignore'){
+            if($name !== '.'
+                && $name !== '..'
+                && $name !== '.gitignore')
+            {
                 $files[] = $name;
             }
             $iterator->next();
         }
+        if(!$extension){
+            $base = [];
+            foreach($files as $file){
+                $base[] = explode('.',$file);
+            }
+            $files = [];
+            foreach($base as $key=>$item){
+                $files[] = $item[0];
+            }
+
+        }
+
 
         return $files;
     }
