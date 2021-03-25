cf21227ed24e7cb5fc559150aa7a730fbd1064d3
root <grzegorzchrymecki@php-developer.ie>
2021-03-25 18:52:55 +0000

    fix calling getProducts on null

--- assets/js/components/cardActions.js
+++ assets/js/components/cardActions.js
@@ -1,3 +1,23 @@
 const AddToCart = document.querySelectorAll('.Card__content__footer--btn');
+let pageOffsetY = localStorage.getItem('offsetY') ?? 0;
+if(pageOffsetY !== 0){
+    window.scrollTo({
+        top: pageOffsetY,
+        left: 0,
+        behavior: 'smooth',
+    });
+}
+setTimeout(function(){
+    pageOffsetY = 0;
+    localStorage.setItem('offsetY', 0);
 
+},2000);
+AddToCart.forEach((addToCartButton)=>{
+   addToCartButton.addEventListener('click', function(e){
+       //e.preventDefault();
+       localStorage.setItem('offsetY', window.pageYOffset);
+
+   });
+
+});
 
