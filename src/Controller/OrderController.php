1e2d1553893d3446fe3c9d4003cd7abbcc89b88b
root <grzegorzchrymecki@php-developer.ie>
2021-03-27 12:32:15 +0000

    ok

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -103,80 +103,32 @@ class OrderController extends AbstractController
                     $user->setPhone($data['phone']);
                     $em->persist($user);
                     $em->flush();
-                    $this->addFlash('success','Registration successful - please Log In redirection after 4s');
+                    $redirectionTime = (int) $this->getParameter('redirectionDelay');
+                    $this->addFlash('success',"Registration successful - please Log In redirection after {$redirectionTime}s");
                     $urlRedirect = $this->generateUrl('app_login');
-                    header("Refresh: 4; {$urlRedirect}");
+                    header("Refresh: {$redirectionTime}; {$urlRedirect}");
 
                 }
 
             }
         }else{
             // method GET
+            $order = new Order();
+            $order->setProductList($cart->getProductList());
+            $order->setProductAmount($cart->getProductAmount());
+            $order->setUuidSession($session->getId());
+            $order->setCreatedAt(new \DateTime());
+            $metaData = $session->getMetadataBag();
+            $expire = $metaData->getCreated() + $metaData->getLifetime();
+            $order->setExpireAt(DateTime::createFromFormat('U', $expire));
+            $order->setIsPaid(false);
+            $order->setUuidSession($session->getId());
             // check is User logged in or NOT
             if($user){
                 //user LOGGED IN
-            }else{
-                //user NOT LOGGED IN
+                $order->setUuidUserId($user->getId());
             }
         }
-        /*
-        $user = $this->getUser();
-        if($request->isMethod('post')){
-            // handling user form
-
-            // create user with Address
-            if(!$isError){
-                // check is user already in DB
-                if(!$user){
-                    $user = $this->getDoctrine()->getRepository(User::class)
-                    ->findOneBy([
-                        'email'=>$data['email'],
-                    ]);
-                }
-
-
-                if(!$user){
-
-
-
-                }
-
-
-                    $manager = $this->getDoctrine()->getManager();
-                    $manager->persist($user);
-                    $manager->flush();
-
-
-                    $manager = $this->getDoctrine()->getManager();
-                    $manager->persist($user);
-                    $manager->flush();
-
-
-
-
-                if($this->getUser()){
-                    $this->addFlash('success', 'Address saved successfully');
-                }
-
-            }
-
-
-        }
-        */
-
-        $order = new Order();
-        $order->setProductList($cart->getProductList());
-        $order->setProductAmount($cart->getProductAmount());
-        $order->setUuidSession($session->getId());
-        if($user = $this->getUser()){
-            $order->setUuidUserId($user->getId());
-        }
-        $order->setCreatedAt(new \DateTime());
-        $metaData = $session->getMetadataBag();
-        $expire = $metaData->getCreated() + $metaData->getLifetime();
-        $order->setExpireAt(DateTime::createFromFormat('U', $expire));
-        $order->setIsPaid(false);
-
 
         return $this->render('order/new.html.twig', [
             'order'=>$order,
