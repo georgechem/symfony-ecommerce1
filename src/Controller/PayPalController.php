162a37b0603704f398e29c1973767573f5a32b5f
root <grzegorzchrymecki@php-developer.ie>
2021-03-28 10:47:42 +0100

    paypal working

--- src/Controller/PayPalController.php
+++ src/Controller/PayPalController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use PayPalCheckoutSdk\Core\PayPalHttpClient;
+use PayPalCheckoutSdk\Core\SandboxEnvironment;
+use PayPalCheckoutSdk\Orders\OrdersCreateRequest;
+use PayPalHttp\HttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +17,38 @@ class PayPalController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('pay_pal/index.html.twig', []);
+        $environment = new SandboxEnvironment($_ENV['PAYPAL_CLIENT_ID'], $_ENV['PAYPAL_SECRET']);
+        $client = new PayPalHttpClient($environment);
+
+        $request = new OrdersCreateRequest();
+        $request->prefer('return=representation');
+        $request->body = [
+            "intent"=>"CAPTURE",
+            "purchase_units"=>[[
+                "reference_id"=>"test_ref_id1",
+                "amount"=>[
+                    "value"=>"1.00",
+                    "currency_code"=>"EUR"
+                ]
+            ]],
+            "application_context"=>[
+                "cancel_url"=>"https://example.com/cancel",
+                "return_url"=>"https://example.com/return"
+            ]
+        ];
+
+        try {
+            $response = $client->execute($request);
+            $links = $response->result->links;
+            //print_r($links);
+        }catch(HttpException $ex){
+            echo $ex->statusCode;
+            print_r($ex->getMessage());
+        }
+
+        //dd('ok');
+        return $this->render('pay_pal/index.html.twig', [
+            'links'=>$links
+        ]);
     }
 }
